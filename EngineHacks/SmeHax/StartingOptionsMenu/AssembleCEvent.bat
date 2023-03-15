9fc0d8ccd88f80bc384454c35997e849ded9fe90
Sme <esahig15789@gmail.com>
2023-03-15 15:20:58 -0400

    ch4 map update

--- EngineHacks/SmeHax/StartingOptionsMenu/AssembleCEvent.bat
+++ EngineHacks/SmeHax/StartingOptionsMenu/AssembleCEvent.bat
@@ -4,10 +4,11 @@ SET startDir=C:\devkitPro\devkitARM\bin\
 SET gcc="%startDir%arm-none-eabi-gcc"
 SET as="%startDir%arm-none-eabi-as"
 SET ref=U:\Projects\FE-CLib\reference\FE8U-20190316
+SET incl=U:\Projects\FE-CLib\include
 SET lyn="C:\devkitPro\lyn.exe"
 
 @rem compile into an object file
-%gcc% -mcpu=arm7tdmi -mabi=aapcs -mthumb -mthumb-interwork -fomit-frame-pointer -ffast-math -fno-toplevel-reorder -mlong-calls -I "U:\Projects\FE-CLib\include" -O2 -c %1 -o "%~n1.o"
+%gcc% -mcpu=arm7tdmi -mabi=aapcs -mthumb -mthumb-interwork -fomit-frame-pointer -ffast-math -fno-toplevel-reorder -mlong-calls -I %incl% -O2 -c %1 -o "%~n1.o"
 
 @rem check for a library s file(called %~n1.lib), and assemble it if needed
 if exist "%ref%.s" (
